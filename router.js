cdd9f96fbcbe9d308b85de9b498dc3ec3f1160f9
Your Name <you@example.com>
2024-02-02 14:15:37 +0100

    first commit

---
+++ router.js
@@ -0,0 +1,30 @@
+import { about } from "./about.js";
+
+const routes = 
+{
+    "home" : "home.html",
+    "about" : "about.html",
+    "projects" : "projects.html",
+    "contact" : "contact.html"
+}
+window.addEventListener("hashchange",()=>{
+    console.log(window.location.hash);
+    navigate()
+})
+
+navigate()
+
+function navigate()
+{
+    let hash = window.location.hash.substring(2)
+    console.log(hash);
+    let page = routes[hash]
+    loadPage(page,document.querySelector("main"))
+}
+
+async function loadPage(url, domObj){
+    const response = await fetch(url)
+    const html = await response.text()
+    domObj.innerHTML=html
+    if(url=="about.html") about()
+}
